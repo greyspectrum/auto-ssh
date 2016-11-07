e5c663d9b87ce663cc326c316568dbdd0ecca11d
greyspectrum <greyspectrum@users.noreply.github.com>
2016-11-07 15:43:09 -0500

    remove ascii art

--- auto-ssh.sh
+++ auto-ssh.sh
@@ -5,14 +5,6 @@
 # -----------
 # Edits your ssh_config to set secure default ciphers, modes, MACs, & settings.
 #
-# 
-#
-#			           FIRE
-#			  ____ 
-#			 (\\\\)= ((O)) > AUTO SSH
-#
-#
-#
 # Chosen defaults are based on original work by Stribika, available here:
 #
 # https://stribika.github.io/2015/01/04/secure-secure-shell.html
